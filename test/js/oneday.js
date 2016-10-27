9cf6e23387a48f8480c87aa31b00d59b21a148b4
superceix <xcjsjkx@163.com>
2016-10-27 15:55:15 +0800

    Site updated: 2016-10-27 15:55:13

--- test/js/oneday.js
+++ test/js/oneday.js
@@ -1,38 +1,99 @@
-var canvas = document.getElementById('canvas');
-var width = canvas.clientWidth;
-var height = canvas.clientHeight;
+var OneDay = function () {
+    this.scene = null;
+    this.renderer = null;
+    this.camera = null;
+    this.width = canvas.clientWidth;
+    this.height = canvas.clientHeight;
 
+    this.Init = function () {
+        this.initRenderer = function () {
+            var canvas = document.getElementById('canvas');
+            this.renderer = new THREE.WebGLRenderer({ canvas: canvas });
+            this.renderer.setClearColor(0xccccccc);
+            this.renderer.setSize(this.width, this.height);
+        }
 
-var renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-});
-renderer.setClearColor(0xccccccc);
-renderer.setSize(width, height);
+        //scene and camera
+        this.initScene = function () {
+            this.scene = new THREE.Scene();
+            this.camera = new THREE.PerspectiveCamera(105, window.innerWidth / window.innerHeight, 0.1, 10000);
+            this.camera.position.set(500, 3600, 1300);
+            this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        }
+        this.initLight = function () {
+            //light
+            var light = new THREE.DirectionalLight(0xffffff);
+            light.position.set(360, 360, 360);
+            this.scene.add(light);
+        }
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.set(50, 360, 800);
+        this._init = function () {
+            this.initRenderer();
+            this.initScene();
+            this.initLight();
 
-var light = new THREE.DirectionalLight(0xffffff);
-light.position.set(360, 360, 360);
-scene.add(light);
+            //坐标轴
+            var axes = new THREE.AxisHelper(10000);
+            this.scene.add(axes);
+        }
+        this._init();
+    }
 
-var texture = THREE.ImageUtils.loadTexture('images/1.jpg', {}, function () {
-    renderer.render(scene, camera);
-});
-texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(11, 10);
-var plane = new THREE.Mesh(new THREE.BoxGeometry(536, 1, 536), new THREE.MeshLambertMaterial({ map: texture }));
+    this.Draw = function () {
+        // 创建一个平面  
+        var planeGeometry = new THREE.PlaneGeometry(10000, 10000, 1, 1);
+        var planeMaterial = new THREE.MeshLambertMaterial({ color: 0xededed });
+        var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.receiveShadow = true;
+        // rotate and position the plane  
+        plane.rotation.x = -0.5 * Math.PI;
+        plane.position.x = 0;
+        plane.position.y = -10;
+        plane.position.z = 0;
+        this.scene.add(plane);
 
-scene.add(plane);
-//scene.add(cube);
+        //画线
+        var material = new THREE.LineBasicMaterial({color: 0x000000});
+        var geometry = new THREE.Geometry();
 
+        geometry.vertices.push(
+            new THREE.Vector3(-2500, 0, 2500),
+            new THREE.Vector3(2500, 0, 2500),
+            new THREE.Vector3(2500, 0, -2500),
+            new THREE.Vector3(-2500, 0, -2500),
+            new THREE.Vector3(-2500, 0, 2500)
+        );
 
-var render = function () {
-    requestAnimationFrame(render);
+        var line = new THREE.Line(geometry, material);
+        this.scene.add(line);
+    }
 
-    plane.rotation.y += 0.02;
-    renderer.render(scene, camera);
+    this.Start = function () {
+        this.Init();
+        this.Draw();
+    }
+    this.Start();
+}
+
+var oneday = new OneDay();
+
+var orbitControls = new THREE.OrbitControls(oneday.camera);
+var clock = new THREE.Clock();
+
+var Render = function () {
+    var delta = this.clock.getDelta();
+    this.orbitControls.update(delta);
+    //plane.rotation.y += 0.02;
+    requestAnimationFrame(Render);
+    oneday.renderer.render(oneday.scene, oneday.camera);
 };
+Render();
 
-render();
+//地面
+// var texture = THREE.ImageUtils.loadTexture('images/1.jpg', {}, function () {
+//     renderer.render(scene, camera);
+// });
+// texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+// texture.repeat.set(11, 10);
+// var ground = new THREE.Mesh(new THREE.BoxGeometry(536, 2, 536), new THREE.MeshLambertMaterial({ map: texture }));
+// scene.add(ground);
